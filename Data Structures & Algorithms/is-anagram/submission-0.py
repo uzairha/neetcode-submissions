35c77d88af6662a8627a0de9c37a09454be3c7c0
uzairha <118949786+uzairha@users.noreply.github.com>
2026-08-28 17:34:50 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_count = [0] * 26
+        t_count = [0] * 26
+        
+
+        for char in s:
+            s_count[ord(char) - ord("a")] += 1
+
+        for char in t:
+            t_count[ord(char) - ord("a")] += 1
+
+        if s_count == t_count:
+            return True
+        
+        return False
